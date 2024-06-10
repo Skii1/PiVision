19205119ac6794e2da2538fb556d5db7a6428462
Alex <amcisaac@me.com>
2024-06-10 09:35:32 -0400

    Added Raspberry Pi running scripts
    Bash shell files have their own respective instructions.

---
+++ Scripts/Scripts/cronscripts/dropcache.sh
@@ -0,0 +1,2 @@
+echo Cache Dropped | sudo rm -r /home/pi/.cache/chromium/Default/Cache/*
+date | sudo tee -a /tmp/log 
---
+++ Scripts/Scripts/cronscripts/refresh.sh
@@ -0,0 +1,5 @@
+export DISPLAY=":0"
+WID=$(xdotool search --onlyvisible --class chromium|head -1)
+xdotool windowactivate ${WID}
+xdotool key ctrl+F5
+echo Tab Refreshed
